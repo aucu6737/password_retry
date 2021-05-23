4247affdbba01d067a436c2a670ddada382cee8b
Winnie Huang <aucu6737@gmail.com>
2021-05-23 14:14:45 +0800

    Updated password_retry.py

--- password_retry.py
+++ password_retry.py
@@ -1,9 +1,14 @@
 password = 'a123456'
-x = 1
-while x <= 3:
-	if input('請輸入密碼: ') == password:
+x = 0
+while x <= 3: 
+	if input('請輸入密碼: ') == password: 
 		print('登入成功')
-		break
+		break 
 	else:
-		print('密碼錯誤! 還有', 3-x, '次機會')
+		print('密碼錯誤!') 
 		x = x + 1
+		if x == 3:
+			print('沒機會嘗試了! 要鎖帳號了! ')
+			break
+		else: 
+			print('還有', 3 - x, '次機會')
